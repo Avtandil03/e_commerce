db18e382279375347659bb657ee58281102bb90e
Avtandil <avtandilqazxswedc@gmail.com>
2023-02-01 19:08:30 +0300

    add commerce.js products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Navbar, Products } from './components';
+import { commerce } from './lib/commerce';
+
+
+
+
 
 const App = () => {
+
+  const [productsIsLoading, setProductsIsLoading] = useState(false)
+
+  const fetchProducts = async() => {
+    const response = await commerce.products.list()
+
+    setProducts(response.data)
+    setProductsIsLoading(true)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const [products, setProducts] = useState()
+
   return (
     <div>
         <Navbar/>
-        <Products/>
+        {productsIsLoading && 
+          <Products products={products}/>
+        }
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,14 +3,13 @@ import {Grid}  from '@material-ui/core'
 import Product from './Product/Product'
 import useStyles from './productsStyles'
 
-const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes.', price:'5$', image: 'https://m.media-amazon.com/images/I/713WdmM3kNL._AC_UL320_.jpg' },
-  { id: 2, name: 'Macbook', description: 'Apple macbook.', price:'10$', image: 'https://m.media-amazon.com/images/I/61ldExf7mbL._AC_UY218_.jpg'}
-]
+const Products = ({products}) => {
 
-const Products = () => {
+
   const classes = useStyles()
-  console.log(classes.content)
+
+  console.log(products)
+  
   return (
     <main className={classes.content}>
       <div className={classes.toolbar}></div>
